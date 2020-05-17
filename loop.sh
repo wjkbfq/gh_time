6de3098dfa15ab7fb09218586c31d658976f11b9
System Administrator <root@m1.local>
2020-05-17 10:39:18 +0900

    git auto commit

--- loop.sh
+++ loop.sh
@@ -1,10 +1,40 @@
+#!/bin/sh
 if [ ! $1 ]; then
-   echo 'ERROR: Need to apply loop param'
-   exit
+	echo 'ERROR: Need to apply loop param'
+	exit
 fi
-for i in $(seq 1 $1)
-do 
-   echo `date +"%Y%m%d_%H%M%S"` >> log.txt && git pull && git commit -a -m 'git auto commit' && git push origin master
-   time=`date +'%G-%m-%d %H:%M:%S' -d '-1 days'`
-   timedatectl set-time "$time"
-done
+echo $1
+
+os_mac(){
+	sudo systemsetup -setusingnetworktime off
+	for i in $(seq 1 $1)
+	do
+		echo `date +"%Y%m%d_%H%M%S"` >> log.txt && git pull && git commit -a -m 'git auto commit' && git push origin master
+		time=`date -v-1d +%m:%d:%Y`
+		sudo systemsetup -setdate "$time"
+		echo $(($1-$i))
+	done
+	sudo systemsetup -setusingnetworktime on
+}
+
+os_linux(){
+	for i in $(seq 1 $1)
+	do
+		# echo `date +"%Y%m%d_%H%M%S"` >> log.txt && git pull && git commit -a -m 'git auto commit' && git push origin master
+		time=`date +'%G-%m-%d %H:%M:%S' -d '-1 days'`
+		timedatectl set-time "$time"
+		echo $(($1-$i))
+	done
+}
+
+os=$(uname -s)
+if [[ "$os" == "Linux" ]]; then
+	os_linux $1
+elif [[ "$os" == "Darwin" ]]; then
+	os_mac $1
+else
+	echo "unknown OS"
+	exit 1
+fi
+
+echo "done"
